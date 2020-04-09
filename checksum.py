55c0c3a79b9afdf54df1950308c47f145ff0aa7c
Matthew O'Neill <mjoneill1988@gmail.com>
2020-04-09 11:26:44 -0400

    test commit

--- checksum.py
+++ checksum.py
@@ -1,14 +1,15 @@
+#Matt O'Neill, Abishek 
 class Checksum:
     def __init__(self):
         pass
-    
+
     @staticmethod
     def compute(data):
         chk_sum = 0
         # Takes in 2 chars
         for i in range(0, len(data), 2):
             if (i+1) < len(data):
-                pos_1 = ord(data[i]) 
+                pos_1 = ord(data[i])
                 pos_2 = ord(data[i+1])
                 chk_sum = chk_sum + (pos_1+(pos_2 << 8))
             elif (i+1)==len(data):
@@ -19,7 +20,7 @@ class Checksum:
         chk_sum = ~chk_sum & 0xffff
 
         return chk_sum
-    
+
     @classmethod
     def verify(cls, data, chk):
         if(cls.compute(data) == chk):
